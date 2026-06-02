77fbcc3c1264a6ce922feb75d59a0e0ad0490856
Hassan <hassan@gmail.com>
2026-06-02 13:02:26 +0600

    done

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks
 
 const verifyToken = async (req, res, next) => {
   const { authorization } = req.headers;
-    console.log(req.headers, 'from verify token');
+  //   console.log(req.headers, 'from verify token');
   const token = authorization?.split(' ')[1];
-    console.log(token);
+  //   console.log(token);
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorize' });
